779430601808be410877a5899a1ba8739f45742b
Vagner Ribeiro <vagn.fedora@gmail.com>
2019-02-13 19:14:01 -0200

    Nova chave da api

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   API_ENDPOINT: '"http://api.navizone.com.br"',
   LOGIN_ENDPOINT: '"http://api.navizone.com.br/v3/oauth"',
-  CLIENT_SECRET: '"Ex9SMxQnJBeqgiPkA2VNJ5FghxurFTX7JWihey9F"',
+  CLIENT_SECRET: '"k3TjMazXEJ99joynCLqvn534Rk85Dv4OSGsADm4r"',
   CLIENT_ID: '1',
   GRANT_TYPE: '"password"',
   RECAPTCHA_SITE_KEY: '"6LeHN4sUAAAAAIx9y6Ku4uGbFWZa7ilmJPlhdqMa"'
