f718dee6ea79c8373f03494b1d47cc12af77dcaa
Vagner Ribeiro <vagn.fedora@gmail.com>
2019-02-13 17:41:14 -0200

    Path to production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT: '"http://mln30.local/v3"',
-  LOGIN_ENDPOINT: '"http://mln30.local/v3/oauth"',
-  CLIENT_SECRET: '"qY4n5MT7X75OpklXttVok8dGtycH6UUUw0asV0pU"',
+  API_ENDPOINT: '"http://navizone.com.br"',
+  LOGIN_ENDPOINT: '"http://navizone.com.br/v3/oauth"',
+  CLIENT_SECRET: '"Ex9SMxQnJBeqgiPkA2VNJ5FghxurFTX7JWihey9F"',
   CLIENT_ID: '1',
   GRANT_TYPE: '"password"',
   RECAPTCHA_SITE_KEY: '"6LeHN4sUAAAAAIx9y6Ku4uGbFWZa7ilmJPlhdqMa"'
