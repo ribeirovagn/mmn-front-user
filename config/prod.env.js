f1d992bad739ffd0526a0ccdb9bd5337dcbff378
Vagner Ribeiro <vagn.fedora@gmail.com>
2019-02-13 17:43:43 -0200

    Path to production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ENDPOINT: '"http://navizone.com.br"',
-  LOGIN_ENDPOINT: '"http://navizone.com.br/v3/oauth"',
+  API_ENDPOINT: '"http://api.navizone.com.br"',
+  LOGIN_ENDPOINT: '"http://api.navizone.com.br/v3/oauth"',
   CLIENT_SECRET: '"Ex9SMxQnJBeqgiPkA2VNJ5FghxurFTX7JWihey9F"',
   CLIENT_ID: '1',
   GRANT_TYPE: '"password"',
