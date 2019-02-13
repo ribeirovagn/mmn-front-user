f7cf3fbbeb122633f10eb9f301ec094ee93edac1
Vagner Ribeiro <vagn.fedora@gmail.com>
2019-02-13 16:11:55 -0200

    Path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/frontend/dist',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
