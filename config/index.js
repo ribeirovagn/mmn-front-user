ca7ea118147ff49dddc0a70461dcc165055fc926
Vagner Ribeiro <vagn.fedora@gmail.com>
2019-02-13 14:44:13 -0200

    A porra do path da production

--- config/index.js
+++ config/index.js
@@ -48,9 +48,9 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '/frontend/dist/static',
-    assetsPublicPath: '/frontend/dist/',
+    assetsRoot: path.resolve(__dirname, '../frontend/dist/'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
